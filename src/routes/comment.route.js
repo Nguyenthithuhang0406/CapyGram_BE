021b391502908acdbf68bc8bab743df8c50a34e4
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-09-03 17:13:02 +0700

    ✨ getCountComments, repliesComment, likeComment, getCountLike

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -12,5 +12,9 @@ const uploadFiles = require('../controllers/upload.controller');
 commentRoute.post('/create', auth, upload.array('mediaComments', 20), uploadFiles, validate(commentValidation.createdComment), commentController.createdComment);
 commentRoute.delete('/delete', auth, validate(commentValidation.deletedComment), commentController.deletedComment);
 commentRoute.get('/getComments/:postId', validate(commentValidation.getComments), commentController.getComments);
+commentRoute.get('/getCountComments/:postId', validate(commentValidation.getCountComments), commentController.getCountComments);
+commentRoute.post('/replies', auth, upload.array('mediaReplies', 20), uploadFiles, validate(commentValidation.repliesComment), commentController.repliesComment);
+commentRoute.put('/like/', auth, validate(commentValidation.likeComment), commentController.likeComment);
+commentRoute.get('/getCountLikes/:commentId', validate(commentValidation.getCountLikes), commentController.getCountLikes);
 
 module.exports = commentRoute;
--- src/validations/comment.validation.js
+++ src/validations/comment.validation.js
@@ -46,8 +46,77 @@ const getComments = {
       }),
   }),
 };
+
+const getCountComments = {
+  params: joi.object({
+    postId: joi.string()
+      .required()
+      .custom(ObjectId)
+      .messages({
+        'any.required': 'postId is required!'
+      }),
+  }),
+};
+
+const repliesComment = {
+  body: joi.object({
+    userId: joi.string()
+      .required()
+      .custom(ObjectId)
+      .messages({
+        'any.required': 'userId is required!'
+      }),
+    commentId: joi.string()
+      .required()
+      .custom(ObjectId)
+      .messages({
+        'any.required': 'commentId is required!'
+      }),
+    postId: joi.string()
+      .required()
+      .custom(ObjectId)
+      .messages({
+        'any.required': 'postId is required!'
+      }),
+    content: joi.string().optional(),
+    newUrls: joi.array().items(joi.string()),
+  }),
+};
+
+const likeComment = {
+  body: joi.object({
+    userId: joi.string()
+      .required()
+      .custom(ObjectId)
+      .messages({
+        'any.required': 'userId is required!'
+      }),
+    commentId: joi.string()
+      .required()
+      .custom(ObjectId)
+      .messages({
+        'any.required': 'commentId is required!'
+      }),
+  }),
+};
+
+const getCountLikes = {
+  params: joi.object({
+    commentId: joi.string()
+      .required()
+      .custom(ObjectId)
+      .messages({
+        'any.required': 'commentId is required!'
+      }),
+  }),
+};
+
 module.exports = {
   createdComment,
   deletedComment,
   getComments,
+  getCountComments,
+  repliesComment,
+  likeComment,
+  getCountLikes,
 };
